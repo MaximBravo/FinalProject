e8b9b7f576b683bd0698a914f31ca7c4a3bbef52
Maxim Bravo <maxim@alexbravo.com>
2017-07-18 15:58:04 -0700

    Step 4: Add Functional Tests - Complete!

--- jokedisplayer/src/main/java/com/maximbravo/jokedisplayer/JokeDisplayActivity.java
+++ jokedisplayer/src/main/java/com/maximbravo/jokedisplayer/JokeDisplayActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -24,6 +25,8 @@ public class JokeDisplayActivity extends AppCompatActivity {
                 String joke = jokeIntent.getStringExtra("joke");
                 jokeTextView.setText(joke);
             }
+        } else {
+            jokeTextView.setVisibility(View.GONE);
         }
     }
 }
